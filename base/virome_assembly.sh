c68ac354a6cf1f43e646a913ae8d9bd2f13917ff
shandley <handley.scott@gmail.com>
2019-11-04 15:29:40 -0600

    First push of base virome assembly script.

--- base/virome_assembly.sh
+++ base/virome_assembly.sh
@@ -28,20 +28,32 @@ for i in $IN/*_R1.s7.out.fastq; do
 # Step 0: Tabulate some kmer statistics
 bbcountunique.sh in=$IN/"$F"_R1.s7.out.fastq in2=$IN/"$F"_R2.s7.out.fastq \
 	interval=2500 \
-	out=$OUT/stats/"$F"_uniq_kmer_stats.txt;
+	out=$OUT/stats/"$F"_uniq_kmer_stats.txt \
+	ow=t;
 
 # Step 1: Digital Normalization
-
 bbnorm.sh in=$IN/"$F"_R1.s7.out.fastq in2=$IN/"$F"_R2.s7.out.fastq extra=$IN/"$F"_singletons.s7.out.fastq \
 	out=$OUT/"$F"_R1.norm.out.fastq out2=$OUT/"$F"_R2.norm.out.fastq outt=$OUT/"$F"_tossed.norm.fastq \
-	fixspikes=t \
-	target=40 mindepth=2 \
+	target=20 mindepth=2 \
 	hist=$OUT/"$F"_norm.hist \
 	ow=t;
 
 # Step 2: Assembly
 megahit -1 $OUT/"$F"_R1.norm.out.fastq -2 $OUT/"$F"_R2.norm.out.fastq -o $OUT/"$F"_megahit_out \
 	--out-prefix "$F".mh;
+
 cp $OUT/"$F"_megahit_out/*.mh.contigs.fa $OUT/megahit_contigs;
 
-done
+# Step 3: Quantification by mapping
+bbmap.sh ref=$OUT/megahit_contigs/"$F".mh.contigs.fa in=$IN/"$F"_R1.s7.out.fastq in2=$IN/"$F"_R2.s7.out.fastq \
+	out=$OUT/megahit_contigs/"$F".aln.sam.gz \
+	kfilter=22 subfilter=15 maxindel=80 \
+	ow=t;
+
+	# Calculate coverage
+	pileup.sh in=$OUT/megahit_contigs/"$F".aln.sam.gz out=$OUT/megahit_contigs/"$F"_cov.txt;
+
+	# Output mappeed/unmapped reads
+	reformat.sh in=$OUT/megahit_contigs/"$F".aln.sam.gz out=$OUT/megahit_contigs/"$F"_mapped.fastq mappedonly;
+     	reformat.sh in=$OUT/megahit_contigs/"$F".aln.sam.gz out=$OUT/megahit_contigs/"$F"_unmapped.fastq unmappedonly;
+done 
