09643496dfe1aea3bc44702ab3b36ed014b6847a
shandley <handley.scott@gmail.com>
2020-03-25 16:10:18 -0500

    Updated convert_phyloseq scripts to use data.table and other memory efficiencies.

--- base/convert_phyloseq_phage.R
+++ base/convert_phyloseq_phage.R
@@ -1,29 +1,38 @@
 #!/usr/bin/Rscript
 
+#### Instructions ###
+# There are several places that require user input. Each section is preceeded with a #### USER INPUT #### tag
+# 1) Need to supply mapping file location for MAP
+# 2) Need to change directroy and filenames to write (two saveRDS commands below)
+# 3) Joining the library size information requires a common key (column name) and should be set in the Standardization section 
+
 # Load libraries
 library("tidyverse")
 library("data.table")
 library("phyloseq")
 library("speedyseq")
 
+#### USER INPUT ####
 # Import mapping file
-MAP <- import_qiime_sample_data("./results/map_phage_attack_virome.txt")
+MAP <- import_qiime_sample_data("./results/mapping_rotabiome_virome.txt")
 
-# Import in viral taxonomy table
-phage_table <- read_tsv(file = "./results/phage_tax_table.tsv", col_names = TRUE, col_types = "ifffffff")
+# Import viral taxonomy table
+viral_table <- fread(file = "./results/phage_tax_table.tsv", header = TRUE, stringsAsFactors=TRUE)
 
-# Remove unfiltered bacerial sequences
-phage_table <- phage_table %>%
-  filter(Kingdom != "Bacteria")
+# Remove any unfiltered bacerial sequences
+setkey(viral_table, Kingdom)
+viral_table <- viral_table[!"Bacteria"]
 
 # Import and adjust count table
 seqtable <- fread(file = "./results/seqtable.all", header = TRUE, sep = "\t", drop = 2)
 seqtable <- setnafill(seqtable, fill=0)
 
-# Merge counts table with tax_table
-stmerge <- left_join(phage_table, seqtable, by = "id")
+# Merge count table with viral tax_table
+setkey(viral_table, id)
+setkey(seqtable, id)
+stmerge <- merge(viral_table, seqtable, all.x = TRUE)
 
-# Sumamrise per taxon counts
+# Sum per taxon counts
 merged_table <- stmerge %>%
   select(-id) %>%
   unite("lineage", c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"), sep = "_") %>%
@@ -32,10 +41,10 @@ merged_table <- stmerge %>%
   separate("lineage", c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"), sep = "_") %>%
   ungroup()
 
-# Remove taxon information to create final counts table (called otu_table in phyloseq)
+# Remove taxon information to create final count table (called otu_table in phyloseq)
 count_table <- merged_table[,-c(1:8)]
 
-# Convert summarized table to a matrix of just the taxon
+# Convert summarized table to a matrix of just the taxon in preperation for phyloseq import
 tax.tbl.m <- merged_table %>%
   select("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species") %>%
   as.matrix()
@@ -45,18 +54,21 @@ OTU = otu_table(count_table, taxa_are_rows = TRUE)
 TAX = tax_table(tax.tbl.m)
 ps0 = phyloseq(OTU, TAX, MAP)
 
-# Write PhyloSeq object as an RDS
+#### USER INPUT ####
+# Save PhyloSeq object as an RDS file
 saveRDS(ps0, file = "./results/ps0_phage.RDS")
 
-##### Alignment Files #####
+# Clean up environment
+rm(count_table, merged_table, OTU, ps0, seqtable.all, stmerge, TAX, tax.tbl.m); gc()
 
+##### Alignment Files #####
 # aa_checked alignment
-aln.aa <- read_delim("./results/aa.aln.m8", col_names = TRUE, delim = "\t")
+aln.aa <- fread(file = "./results/aa.aln.m8", header = TRUE, sep = "\t")
 aln.aa <- aln.aa %>%
   rename(id = query)
 
 # nt_checked alignment
-aln.nt <- read_delim("./results/nt.aln.m8", col_names = TRUE, delim = "\t")
+aln.nt <- fread(file = "./results/nt.aln.m8", header = TRUE, sep = "\t")
 aln.nt <- aln.nt %>%
   rename(id = query)
 
@@ -67,21 +79,26 @@ aln.nt <- aln.nt %>%
   mutate(query_type = "nt")
 
 aln.all <- bind_rows(aln.aa, aln.nt)
+aln.all <- data.table(aln.all)
 
-# Merge viral taxonomy with aa alignment table
-vir.aln <- left_join(phage_table, aln.all, by = "id")
+# Merge viral taxonomy and aa/nt alignment table
+setkey(viral_table, id)
+setkey(aln.all, id)
+vir.aln <- merge(viral_table, aln.all, all.x = TRUE)
 
-# Merge virus + alignment table with seqtable
-aln.merge <- left_join(vir.aln, seqtable, by = "id")
+# Merge virus + alignment table with count table
+setkey(seqtable, id)
+aln.merge <- merge(vir.aln, seqtable, all.x = TRUE)
 
 # Extract count table
 aln.count <- aln.merge[,-c(1:20)]
 OTU.ALN <- otu_table(aln.count, taxa_are_rows = TRUE)
 
-# Create tax table
+# Extract tax table
 aln.tax.tbl <- aln.merge[,c(1:20)] %>%
   as_tibble()
 
+# Convert to matrix in preperation for PhyloSeq
 aln.tax.tbl.m <- aln.merge[,c(1:20)] %>%
   as.matrix()
 
@@ -91,40 +108,57 @@ TAX.ALN <- tax_table(aln.tax.tbl.m)
 ps.aln <- phyloseq(OTU.ALN, TAX.ALN, MAP)
 
 # Adjust PhyloSeq alignment object
+# This may take a lot of time and require a lot of RAM
 ps.aln.adj <- ps.aln %>%
   speedyseq::psmelt() %>%
   as_tibble()
 
+# Adjust values
 ps.aln.value <- ps.aln.adj %>%
   select(c((ncol(MAP)+13):(ncol(ps.aln.adj)-1))) %>%
    mutate_if(is.factor, ~ as.numeric(levels(.x))[.x])
 
+# Adjust factors
 ps.aln.fact <- ps.aln.adj %>%
   select(c(1:(ncol(MAP)+12)), ncol(ps.aln.adj)) # include query type (last column)
 
 ps.aln.fixed <- bind_cols(ps.aln.fact, ps.aln.value)
 
-# Add UniProt definitions
+# Convert to data table. This may take a bit of time as well
+ps.aln.fixed <- data.table(ps.aln.fixed)
+
+# Free up some memory
+rm(aln.aa, aln.all, aln.count, aln.merge, aln.nt, aln.tax.tbl, aln.tax.tbl.m, OTU.ALN, ps.aln, ps.aln.adj, ps.aln.fact, ps.aln.value, TAX.ALN, vir.aln, viral_table); gc()
+
+# Add UniProt target descriptions
 descriptions <- fread("/mnt/data1/databases/virus_uniprot/uniprot.description.db", stringsAsFactors = TRUE, col.names = c("target", "protein"))
-ps.aln.fixed <- left_join(ps.aln.fixed, descriptions, by = "target")
-ps.aln.fixed$target <- as.factor(ps.aln.fixed$target) # left_join may have converted to character
+setkey(ps.aln.fixed, target)
+setkey(descriptions, target)
+ps.aln.fixed <- merge(ps.aln.fixed, descriptions, all.x = TRUE)
+rm(descriptions); gc()
 
-# Add Baltimore classifications
+# Add Baltimore virus classifications
 baltimore <- fread("/mnt/data1/databases/hecatomb/2020_03_11_family_Baltimore_classification_lookup.txt", stringsAsFactors = TRUE)
-ps.aln.fixed <- left_join(ps.aln.fixed, baltimore, by = "Family")
-ps.aln.fixed$Family <- as.factor(ps.aln.fixed$Family) # left_join may have converted to character
+setkey(ps.aln.fixed, Family)
+setkey(baltimore, Family)
+ps.aln.fixed <- merge(ps.aln.fixed, baltimore, all.x = TRUE)
+rm(baltimore); gc()
 
-# Standardize
+# Standardize to per sample library size
 col.sums <- as_tibble(colSums(seqtable[,-1]))
 samples <- as_tibble(colnames(seqtable[,-1]))
 library.size <- bind_cols(samples, col.sums)
+rm(col.sums, samples); gc()
 
+#### USER INPUT ####
 library.size <- library.size %>%
-  rename(SampleID = value) %>%
+  rename(seq_id = value) %>% # Rename to whatever your sequence file name / sample name is in your mapping file
   rename(library_size = value1)
+library.size <- data.table(library.size)
 
-ps.aln.fixed <- inner_join(ps.aln.fixed, library.size, by = "SampleID")
-ps.aln.fixed$SampleID <- as.factor(ps.aln.fixed$SampleID) # join may have converted to character
+setkey(ps.aln.fixed, seq_id)
+setkey(library.size, seq_id)
+ps.aln.fixed <- ps.aln.fixed[library.size, nomatch = 0]
 
 # Add proportion, min and mean and median standardizations
 min.ls <- min(library.size$library_size)
@@ -137,6 +171,15 @@ ps.aln.scaled <- ps.aln.fixed %>%
 	mutate(scaled_abundance_mean = (mean.ls*proportional_abundance)) %>%
 	mutate(scaled_abundance_median = (median.ls*proportional_abundance))
 
-# Write alignment tables as PhyloSeq object
-saveRDS(ps.aln.fixed, file = "./results/ps_aln_phage.RDS")
+# Adjust nt alignment length
+ps.aln.scaled <- ps.aln.scaled %>%
+	mutate(alignment_length_adjusted = ifelse(query_type == "aa", alignment_length*3, alignment_length*1))
 
+#### USER INPUT ####
+# Write alignment table
+saveRDS(ps.aln.fixed, file = "./results/aln_phage.RDS")
+
+# Write zero abundance filtered file
+ps.aln.fixed.filt <- ps.aln.fixed %>%
+	filter(Abundance > 0)
+saveRDS(ps.aln.fixed.filt, file = "./results/aln_phage_filt.RDS")
