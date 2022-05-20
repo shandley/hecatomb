a60aca257461a027ac008391aa1ee8962d971f4d
Michael Roach <beardymcjohnface@gmail.com>
2022-05-20 18:10:58 +0930

    more lint fixes

--- snakemake/workflow/scripts/kronaText.py
+++ snakemake/workflow/scripts/kronaText.py
@@ -15,14 +15,16 @@ logging.basicConfig(filename=snakemake.log[0], filemode='w', level=logging.DEBUG
 
 logging.debug('Slurping Tax assignments from bigtable')
 counts = {}
-for l in stream_tsv(snakemake.input[0]):
-    if l[0] == "seqID":
-        continue
-    t = '\t'.join(l[23:30])
-    try:
-        counts[t] += int(l[2])
-    except KeyError:
-        counts[t] = int(l[2])
+with open(snakemake.input[0], 'r') as fh:
+    for line in fh:
+        l = line.strip().split('\t')
+        if l[0] == "seqID":
+            continue
+        t = '\t'.join(l[23:30])
+        try:
+            counts[t] += int(l[2])
+        except KeyError:
+            counts[t] = int(l[2])
 logging.debug('Sorting, counting, and writing tax assignments')
 outFH = open(snakemake.output[0], 'w')
 for k in sorted(counts.keys()):
