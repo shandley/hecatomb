490d61c4de49abc9eee29f9553bb6140fcc2dec8
Michael Roach <beardymcjohnface@gmail.com>
2024-01-30 17:39:57 +1030

    MAINT: Add unit tests for Hecatomb

---
+++ tests/test_hecatomb.py
@@ -0,0 +1,50 @@
+import subprocess
+from pathlib import Path
+
+import pytest
+
+
+TEST_ROOTDIR = Path(__file__).parent
+EXEC_ROOTDIR = Path(__file__).parent.parent
+
+
+@pytest.fixture(scope="session")
+def tmp_dir(tmpdir_factory):
+    return tmpdir_factory.mktemp("tmp")
+
+
+@pytest.fixture(autouse=True)
+def workingdir(tmp_dir, monkeypatch):
+    """set the working directory for all tests"""
+    monkeypatch.chdir(tmp_dir)
+
+
+def exec_command(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE):
+    """executes shell command and returns stdout if completes exit code 0
+    Parameters
+    ----------
+    cmnd : str
+      shell command to be executed
+    stdout, stderr : streams
+      Default value (PIPE) intercepts process output, setting to None
+      blocks this."""
+
+    proc = subprocess.Popen(cmnd, shell=True, stdout=stdout, stderr=stderr)
+    out, err = proc.communicate()
+    if proc.returncode != 0:
+        raise RuntimeError(f"FAILED: {cmnd}\n{err}")
+    return out.decode("utf8") if out is not None else None
+
+
+def test_hecatomb_cli(tmp_dir):
+    exec_command("hecatomb -v")
+    exec_command("hecatomb -h")
+    exec_command("hecatomb run -h")
+    exec_command("hecatomb config -h")
+
+
+def test_koverage_commands(tmp_dir):
+    exec_command("hecatomb test --threads 1 -n")
+    exec_command("hecatomb hecatomb install -n")
+    exec_command("hecatomb config")
+    exec_command("hecatomb citation")
